7f41b4ce12813dc1c3bf1f6643d26274869bd99a
lizherui <lzrak47m4a1@gmail.com>
2013-07-31 12:00:17 +0800

    ci by lizherui

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 spider_python
 =============
 
-爬虫，爬虫。
+抓取北邮人论坛和水木社区校招信息的爬虫程序, 非常简洁，可以扩展。
+
+爬虫根据自定义关键字先对校招信息进行过滤，然后存储到本机redis中。本机若有lamp环境，可直接从redis读取信息到web页面上即可，lamp环境中的php程序示例如下：
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Welcome to spider!</title>
+    <style>
+        body {
+            width: 35em;
+            margin: 0 auto;
+        }
+        a:visited { color: red; }
+    </style>
+    </head>
+    <body>
+    <?php
+    $rs_ip = '127.0.0.1';
+    $rs_port = 6379;
+    $rs = new Redis();
+    $rs->connect($rs_ip, $rs_port);
+    $ret = $rs->smembers('urls');
+    foreach($ret as $herf) {
+        echo $herf . "<br/>";
+    }
+    ?>
+    </body>
+    </html>
+
+效果截图：
+    
+![1](https://lh3.googleusercontent.com/-mqsrIBbWj4A/UfiLMw4sW2I/AAAAAAAAAGE/_IHC__pJVxE/w958-h190-no/%25E5%25B1%258F%25E5%25B9%2595%25E5%25BF%25AB%25E7%2585%25A7+2013-07-31+%25E4%25B8%258A%25E5%258D%258811.56.48.png)
+
+Enjoy it。
+
--- main.py
+++ main.py
@@ -19,6 +19,44 @@ import redis
 import logging
 from BeautifulSoup import BeautifulSoup
 
+LOG_ADDRESS = '/usr/local/var/log/spider_logging.txt'                   #日志文件地址
+LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'     #日志格式
+LOG_LEVEL = logging.DEBUG                                               #日志级别
+REDIS_IP = '127.0.0.1'                                                  #Redis的ip
+REDIS_PORT = 6379                                                       #Redis的port
+REDIS_FREQUENCE = 10                                                    #Redis清空的频率
+SPIDER_KEYS = (u'校招', u'应届', u'毕业生')                             #筛选的关键词
+
+def init_log():
+    logger = logging.getLogger() 
+    handler = logging.FileHandler(LOG_ADDRESS)
+    formatter = logging.Formatter(LOG_FORMAT) 
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(LOG_LEVEL)
+    return logger
+
+def init_params():
+    return (
+            {
+                'host' : 'http://bbs.byr.cn',
+                'url'  : 'http://bbs.byr.cn/board/JobInfo',
+                'headers' : {
+                    "X-Requested-With" : "XMLHttpRequest",
+                },
+                'href' : "^/article/JobInfo/\d+$",
+            },
+
+            {
+                'host' : 'http://www.newsmth.net',
+                'url'  : 'http://www.newsmth.net/nForum/board/Career_Campus',
+                'headers' : {
+                    "X-Requested-With" : "XMLHttpRequest",
+                },
+                'href' : "^/nForum/article/Career_Campus/\d+$",
+            },
+        )
+
 def spider(rs, host, url, headers, href):
     r = requests.get(url, headers = headers)
     frs_soup = BeautifulSoup(r.text)
@@ -29,54 +67,29 @@ def spider(rs, host, url, headers, href):
     }
     frs_res =  frs_soup.findAll('a', frs_attrs)
     for line in frs_res:
+        #去除置顶贴
         if line.parent.parent.get('class') == 'top':
             continue
         line['href'] = host + line['href']
         title = line.string
-        keys = (u'校招', u'应届', u'毕业生')
-        if filter(lambda x: x in title, keys):
+        if filter(lambda x: x in title, SPIDER_KEYS):
             rs.sadd('urls', line)
 
 def main():
-    logger=logging.getLogger() 
-    handler=logging.FileHandler("/usr/local/var/log/spider_logging.txt")
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s') 
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-    logger.info('start!')
+    logger = init_log()
+    logger.info('spider start!')
 
-    rs_host = 'localhost'
-    rs_port = 6379
-    rs = redis.Redis(host=rs_host, port=rs_port)
-
+    rs = redis.Redis(host=REDIS_IP, port=REDIS_PORT)
     rs.incr('times')
-    if int(rs.get('times')) >= 10:
+    if int(rs.get('times')) >= REDIS_FREQUENCE:
         rs.flushall()
 
-    params = (
-        {
-            'host' : 'http://bbs.byr.cn',
-            'url'  : 'http://bbs.byr.cn/board/JobInfo',
-            'headers' : {
-                "X-Requested-With" : "XMLHttpRequest",
-            },
-            'href' : "^/article/JobInfo/\d+$",
-        },
-
-        {
-            'host' : 'http://www.newsmth.net',
-            'url'  : 'http://www.newsmth.net/nForum/board/Career_Campus',
-            'headers' : {
-                "X-Requested-With" : "XMLHttpRequest",
-            },
-            'href' : "^/nForum/article/Career_Campus/\d+$",
-        },
-    )
+    params = init_params()
 
     for param in params :
         spider(rs, param['host'], param['url'], param['headers'], param['href'])
-    logger.info("end!\n") 
+
+    logger.info("spider finish!\n") 
 
 if __name__ == '__main__':
     main()
